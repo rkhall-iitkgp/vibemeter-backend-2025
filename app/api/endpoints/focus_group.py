ca6387fd38369ef31f4bb5c7c06adf943e7ff067
Ankan <93087057+ItsAnkan@users.noreply.github.com>
2025-04-03 01:32:07 +0530

    feat:add focus groups and apis

---
+++ app/api/endpoints/focus_group.py
@@ -0,0 +1,191 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from typing import List, Optional
+from pydantic import BaseModel
+from datetime import datetime
+from fastapi.responses import JSONResponse
+
+from app.utils.db import get_db
+from app.utils.helpers import format_response
+from app.models.schema import FocusGroup, User
+
+router = APIRouter()
+
+class UserData(BaseModel):
+    employee_id: str
+    email: str
+    password: str
+    is_verified: bool
+    profile_picture: str
+
+class GroupCreate(BaseModel):
+    name: str
+    description: str
+    tags: List[str]
+    users: List[str]
+
+
+class GroupData(BaseModel):
+    focus_group_id: str
+    name: str
+    description: str
+    created_at: datetime
+    tags: List[str]
+    users: List[UserData]
+    actions: List
+    surveys: List
+
+
+@router.get("")
+async def get_all_groups(db: Session = Depends(get_db)):
+    """
+    Fetch all focus groups from the database.
+    """
+    try:
+        groups = db.query(FocusGroup).all()
+        return format_response(data=groups)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to retrieve focus groups: {str(e)}",
+        )
+
+
+@router.get("/{focus_group_id}")
+async def get_group_details(focus_group_id: str, db: Session = Depends(get_db)):
+    """
+    Fetch focus group data from the database.
+    """
+    try:
+        group = (
+            db.query(FocusGroup)
+            .filter(FocusGroup.focus_group_id == focus_group_id)
+            .first()
+        )
+        if not group:
+            raise HTTPException(status_code=404, detail="Focus Group not found.")
+
+        users_data = []
+        for user in group.users:
+            users_data.append(
+                {
+                    "employee_id": user.employee_id,
+                    "email": user.email,
+                    "is_verified": user.is_verified,
+                    "profile_picture": user.profile_picture,
+                }
+            )
+
+        formatted_group = {
+            "focus_group_id": group.focus_group_id,
+            "name": group.name,
+            "description": group.description,
+            "created_at": group.created_at,
+            "tags": group.tags,
+            "users": users_data,
+            "actions": group.actions,
+            "surveys": group.surveys,
+        }
+        return format_response(data=formatted_group)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to retrieve group: {str(e)}",
+        )
+
+
+@router.post("")
+async def create_group(group: GroupCreate, db: Session = Depends(get_db)):
+    """
+    Create a new focus group in the database.
+    """
+    try:
+        # Create the group with the actual FocusGroup objects
+        new_group = FocusGroup(
+            name=group.name,
+            description=group.description,
+            tags=group.tags,
+        )
+
+        for employee_id in group.users:
+            user = db.query(User).filter(User.employee_id == employee_id).first()
+            if not user:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"Employee with ID {employee_id} not found",
+                )
+            new_group.users.append(user)
+
+        db.add(new_group)
+        db.commit()
+        db.refresh(new_group)
+
+        # Return the formatted response
+        return format_response(data=new_group)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to create focus group: {str(e)}",
+        )
+
+
+@router.put("/{focus_group_id}")
+async def update_group(
+    focus_group_id: str, question: GroupData, db: Session = Depends(get_db)
+):
+    """
+    Update an existing focus group in the database.
+    """
+    try:
+        db_group = (
+            db.query(FocusGroup).filter(FocusGroup.focus_group_id == focus_group_id).first()
+        )
+        if not db_group:
+            raise HTTPException(status_code=404, detail="Focus Group not found.")
+
+        for key, value in question.dict().items():
+            setattr(db_group, key, value)
+
+        db.commit()
+        db.refresh(db_group)
+        return format_response(data=db_group)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to update focus group: {str(e)}",
+        )
+
+
+@router.delete("/{focus_group_id}")
+async def delete_group(focus_group_id: str, db: Session = Depends(get_db)):
+    """
+    Delete a focus group from the database.
+    """
+    try:
+        db_group = (
+            db.query(FocusGroup).filter(FocusGroup.focus_group_id == focus_group_id).first()
+        )
+        if not db_group:
+            raise HTTPException(status_code=404, detail="Focus Group not found.")
+
+        db.delete(db_group)
+        db.commit()
+        return JSONResponse(
+            content={"message": "Focus Group Deleted successfully."},
+            status_code=status.HTTP_204_NO_CONTENT,
+        )
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to delete focus group: {str(e)}",
+        )
